b04c22ec7c7edec8022c68a370e549f224b6a5eb
Tadon <Mark.Klocek@GMail.com>
2025-04-09 15:05:55 -0700

    leetcode_easy_two_sum first solution of O(n^2), then later revised to O(n)

--- leetcode_easy_two_sum.py
+++ leetcode_easy_two_sum.py
@@ -15,6 +15,8 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 '''
 
+'''
+O(n^2)
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -22,3 +24,40 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        first_num = None
+        second_num = None 
+        for i in range(len(nums)):
+            
+            if target-nums[i] in nums[i+1:]:
+                first_num = i
+            if first_num != None and nums[first_num] + nums[i] == target:
+                second_num = i
+                
+                
+                
+
+        return first_num, second_num
+        '''
+
+#correct, O(n) time solution:
+class Solution(object):
+    def twoSum(self, nums, target):
+        
+        empty_dict = {}
+        for i in range(len(nums)):
+            if target - nums[i] in empty_dict:
+                return empty_dict[target-nums[i]], i
+            else:
+                empty_dict[nums[i]] = i
+        return "Error"
+
+print(Solution().twoSum([2, 7, 11, 15], 9))        # [0, 1]
+print(Solution().twoSum([3, 2, 4], 6))             # [1, 2]
+print(Solution().twoSum([3, 3], 6))                # [0, 1]
+print(Solution().twoSum([1, 5, 3, 6], 9))          # [1, 3]
+print(Solution().twoSum([1, 2, 3, 4, 5, 6], 7))    # [0, 5] or [1, 4] depending on how you handle it
+print(Solution().twoSum([0, 4, 3, 0], 0))          # [0, 3]
+print(Solution().twoSum([-3, 4, 3, 90], 0))        # [0, 2]
+print(Solution().twoSum([1, 2, 5, 6], 10))         # "Error"
+print(Solution().twoSum([2, 5, 5, 11], 10))        # [1, 2]
+print(Solution().twoSum([5, 75, 25], 100))         # [1, 2]
