b6c0eaebea992908f4dcb034ed0dfbf965fc4de5
Tadon <Mark.Klocek@GMail.com>
2025-04-09 17:29:54 -0700

    leetcode_medium_group_anagram added frequency array solution

--- leetcode_medium_group_anagrams.py
+++ leetcode_medium_group_anagrams.py
@@ -27,8 +27,9 @@ Output: [[""]]
 
 
 '''
+#Sorted solution
 
-class Solution:
+'''class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         new_dict = {}
         
@@ -39,4 +40,22 @@ class Solution:
             else:
                 new_dict[key] = [word]
         return new_dict.values()
-        
+        '''
+
+#Frequency array solution
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        new_dict = {}
+        for word in strs:
+            freq = [0] * 26
+            for letter in word:
+                letter_index = ord(letter) - ord('a')
+                freq[letter_index] += 1
+            key = tuple(freq)
+            if key in new_dict:
+                new_dict[key].append(word)
+            else:
+                new_dict[key] = [word]
+        return new_dict.values()
+                
